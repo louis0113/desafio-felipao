4db0aa3c4e4bf949c321dd483fb0d4d08094aa61
louis0113 <luizhenrique011304@gmail.com>
2024-12-01 18:49:48 -0300

    Finalizando o desafio do Felipão

--- DesafioBoss/boss.js
+++ DesafioBoss/boss.js
@@ -1,5 +1,51 @@
 const prompt = require("prompt-sync")()
 
-const teste = prompt("Digite algo: ")
-console.log(teste)
+const nome = prompt("Digite seu nome, heroi: ")
+
+const xp = Number(prompt(`Digite a quantidade de XP atual, ${nome}: `))
+
+const classificacao = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascedente", "Imortal", "Radiante"]
+
+let nivelHeroi
+
+function buscarNivel(limite){
+    for(let i = 0; i < limite; i++){
+        nivelHeroi = classificacao[i]
+        
+    }
+}
+ 
+        
+        switch (true){
+            case xp < 1000 :
+                buscarNivel(1)
+            break      
+            case xp <= 2000:
+                 buscarNivel(2)
+            break     
+            case xp <= 5000:
+                buscarNivel(3)
+            break     
+            case xp <= 7000:
+                buscarNivel(4)    
+            break;    
+            case xp <= 8000:
+                buscarNivel(5)
+            break;
+            case xp <= 9000:
+                buscarNivel(6)
+            break;
+            case xp <= 10000:
+                buscarNivel(7)
+            break;
+            case xp > 10000:
+                buscarNivel(8)      
+        }
+    
+        
+    
+
+
+console.log(`O héroi de nome ${nome} está no nível ${nivelHeroi}`)
+
 
